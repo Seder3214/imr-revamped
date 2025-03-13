6ed0176aa386aa36a78072bccf0e541a9c5177e2
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2025-03-13 23:12:25 +0300

    fix saving

--- js/saves.js
+++ js/saves.js
@@ -458,9 +458,9 @@ function cannotSave() { return tmp.supernova.reached && player.supernova.times.l
 function save(){
     let str = btoa(JSON.stringify(player))
     if (cannotSave() || findNaN(str, true)) return
-    if (localStorage.getItem("testSave") == '') wipe()
-    localStorage.setItem("testSave",str)
-    tmp.prevSave = localStorage.getItem("testSave")
+    if (localStorage.getItem("revampSave") == '') wipe()
+    localStorage.setItem("revampSave",str)
+    tmp.prevSave = localStorage.getItem("revampSave")
     if (tmp.saving < 1) {addNotify("Game Saved", 3); tmp.saving++}
 }
 
@@ -548,7 +548,7 @@ function importy() {
 }
 
 function loadGame(start=true, gotNaN=false) {
-    if (!gotNaN) tmp.prevSave = localStorage.getItem("testSave")
+    if (!gotNaN) tmp.prevSave = localStorage.getItem("revampSave")
     wipe()
     load(tmp.prevSave)
     
